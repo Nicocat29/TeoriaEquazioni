3e9cc54ae9949be476cf63e093508d112a18384e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-02-06 08:59:40 +0100

    Restituisce true se l'equazione è determinata

---
+++ EquazioniLibrary.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EquazioniLibrary.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EquazioniLibrary.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1c36e7ce-9d70-4833-973c-86658a5a19cf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquazioniLibrary;
+
+namespace EquazioniLibrary.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double a = 3;
+            bool risp_aspettata = true;
+            bool risp = Equazioni.IsDetermined(a);
+            Assert.AreEqual(risp_aspettata, risp);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double a = -3;
+            bool risp_aspettata = true;
+            bool risp = Equazioni.IsDetermined(a);
+            Assert.AreEqual(risp_aspettata, risp);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            double a = 0;
+            bool risp_aspettata = false;
+            bool risp = Equazioni.IsDetermined(a);
+            Assert.AreEqual(risp_aspettata, risp);
+        }
+    }
+}
